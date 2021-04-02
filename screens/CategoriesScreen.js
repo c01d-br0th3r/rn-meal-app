92972545bb077c1db7bcb1c8e5d45024b3d8efcc
c01d-br0th3r <60615552+c01d-br0th3r@users.noreply.github.com>
2021-04-02 17:44:12 +0900

    feat(screens): add screens

---
+++ screens/CategoriesScreen.js
@@ -0,0 +1,68 @@
+import React from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  FlatList,
+  TouchableOpacity,
+} from "react-native";
+import CategoryGridTile from "../components/CategoryGridTile";
+import { HeaderButtons, Item } from "react-navigation-header-buttons";
+
+import { CATEGORIES } from "../data/dummy-data";
+import CustomHeaderButton from "../components/HeaderButton";
+
+const CategoriesScreen = (props) => {
+  const renderGridItem = (itemData) => {
+    return (
+      <CategoryGridTile
+        title={itemData.item.title}
+        color={itemData.item.color}
+        onSelect={() => {
+          props.navigation.navigate({
+            routeName: "CategoryMeals",
+            params: {
+              categoryId: itemData.item.id,
+            },
+          });
+        }}
+      />
+    );
+  };
+
+  return (
+    <FlatList
+      keyExtractor={(item, idx) => item.id}
+      data={CATEGORIES}
+      renderItem={renderGridItem}
+      numColumns={2}
+    />
+  );
+};
+
+CategoriesScreen.navigationOptions = (navigationData) => {
+  return {
+    headerTitle: "카테고리",
+    headerLeft: () => (
+      <HeaderButtons HeaderButtonComponent={CustomHeaderButton}>
+        <Item
+          title="Menu"
+          iconName="ios-menu"
+          onPress={() => {
+            navigationData.navigation.toggleDrawer();
+          }}
+        />
+      </HeaderButtons>
+    ),
+  };
+};
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
+
+export default CategoriesScreen;
---
+++ screens/FavoritesScreen.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+import MealList from "../components/MealList";
+import { MEALS } from "../data/dummy-data";
+import { HeaderButtons, Item } from "react-navigation-header-buttons";
+import CustomHeaderButton from "../components/HeaderButton";
+
+const FavoriteScreen = (props) => {
+  const favMeals = MEALS.filter((meal) => meal.id === "m1" || meal.id === "m2");
+  return <MealList listData={favMeals} navigation={props.navigation} />;
+};
+
+FavoriteScreen.navigationOptions = (navigationData) => {
+  return {
+    headerTitle: "Favorites",
+    headerLeft: () => (
+      <HeaderButtons HeaderButtonComponent={CustomHeaderButton}>
+        <Item
+          title="Menu"
+          iconName="ios-menu"
+          onPress={() => {
+            navigationData.navigation.toggleDrawer();
+          }}
+        />
+      </HeaderButtons>
+    ),
+  };
+};
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
+
+export default FavoriteScreen;
