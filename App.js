f1bd3493afe2651ce445bd7b9279ca0ede7870e2
c01d-br0th3r <60615552+c01d-br0th3r@users.noreply.github.com>
2021-04-02 17:44:29 +0900

    feat(App.js): apply navigation in App.js

--- App.js
+++ App.js
@@ -1,21 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { useFonts } from "expo-font";
+import AppLoading from "expo-app-loading";
+import { enableScreens } from "react-native-screens";
+
+import MealsNavigator from "./navigation/MealsNavigator";
+
+enableScreens();
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+  let [fontLoaded] = useFonts({
+    "open-sans-bold": require("./assets/fonts/OpenSans-Bold.ttf"),
+    "open-sans": require("./assets/fonts/OpenSans-Regular.ttf"),
+  });
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  if (!fontLoaded) {
+    return <AppLoading />;
+  } else {
+    return <MealsNavigator />;
+  }
+}
